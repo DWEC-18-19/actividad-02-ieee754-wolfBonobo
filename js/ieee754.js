f9ccb2467fe2e2e23f46bf79761efee47885caf6
wolfBonobo <wolfBonobo@users.noreply.github.com>
2018-11-03 16:28:23 +0100

    Version 1.0.0

--- js/ieee754.js
+++ js/ieee754.js
@@ -2,15 +2,19 @@
 
 function ieee754aBinario(numero) {
 
-    var decimalStr = numero;
-    var signo;
-    var entero;
-    var decimales;
-    var binario;
+    let decimalStr = numero;
+    let signo;
+    let entero;
+    let decimales;
+    let binario;
+
+    //Verifico si se han introducion comillas dobles y las elimino
+    while (decimalStr.indexOf("\"") != -1)
+        decimalStr = decimalStr.toString().replace("\"", "");
+
 
     //Averiguo signo si número introducido es positivo o nevativo y quito signo
     // 0 = + ||  1 = -
-
     if (parseFloat(decimalStr) > 0) {
         signo = 0;
     } else {
@@ -20,56 +24,103 @@ function ieee754aBinario(numero) {
 
     //verifico si de entrada el número tiene decimales o no
     if (decimalStr.indexOf(".") != -1) {
-
         //Saco parte entera de decimalStr   
         entero = decimalStr.substring(0, decimalStr.indexOf("."));
-
         //Saco decimales de decimalStr   
-        decimales = decimalStr.substring((decimalStr.indexOf(".") + 1), (decimalStr.length));
-        binario = decimalBinarioParser(entero) +  decimalFraccionarioBinarioParser(decimales);
+        decimales = decimalStr.substring((decimalStr.indexOf(".")), (decimalStr.length));
 
+        //Utilizo métodos auxiliares para sacar número binario entero y número binario decimal fraccionario.
+        binario = decimalBinarioParser(entero) + decimalFraccionarioBinarioParser("0" + decimales);
     } else {
-
         binario = decimalBinarioParser(decimalStr);
     }
+    //utilizo método parsea de binarioa  notación científica a ieee754
+    numero = binarioIeee754aParser(signo, binario);
 
-    //por ahora soolo devuelve parte decimal entera a binario
-    //Pendiente implementar metodo decimalFraccionarioBinarioParser()
-    //Pendiente implementar  metodo binarioNCientificaParser().
-    //notacionCientificaIeee754aParser
-    numero = binario;
     return numero;
 }
 
 
 
-//====================
-//Funciones auxiliares 
-//====================
+//================================
+//      FUNCIONES AUXILIARES
+//================================
 
 
-//Parsea decimal a binario OK
+//Parsea decimal a binario 
+//---------------------------
 function decimalBinarioParser(n) {
-    var decimal = parseInt(n);
-    var binario = "";
-    var resto;
+    let decimal = parseInt(n);
+    let binario = "";
+    let resto;
 
     do {
         resto = decimal % 2;
         binario = resto.toString() + binario;
         decimal = parseInt(decimal / 2);
-    } while (decimal > 0)
+    } while (decimal > 0);
+
+    //console.log(binario);
 
     return binario;
 }
-//Parsea decimal fraccionario  a binario
+
+
+//Parsea decimal fraccionario  a binario 
+//--------------------------------------
 function decimalFraccionarioBinarioParser(n) {
 
-    //sin implementar
-    return "."+n;
+    let decinalFraccionarioBinario = "";
+    let decimalFraccionario = n;
+
+    //Calculo de la precisión del decimál fraccionario será 21
+    for (let i = 0; i < 21; i++) {
+
+        decimalFraccionario = decimalFraccionario * 2;
+
+        if (decimalFraccionario >= 1) {
+            decinalFraccionarioBinario += "1";
+            decimalFraccionario = decimalFraccionario - 1;
+        } else {
+            decinalFraccionarioBinario += "0";
+        }
+
+    }
+
+    return "." + decinalFraccionarioBinario;
 
 }
-//Parses de binario a  Notación Científica 
-function binarioNCientificaParser(){}
-//Parses de  Notación Científica a IEEE-754
-function notacionCientificaIeee754Parser(){}
+
+
+//Parses de binario a  Notación Científica a  ieee75432bits
+function binarioIeee754aParser(sig, bi) {
+
+    let ieee75432bits;
+    //Averiguo potencia de 2 para calculo exponenente
+    let potencia2 = (bi.toString().indexOf(".") - 1);
+    let nCientifica = bi.replace(".", "");
+    let mantisa = nCientifica.substring(1, nCientifica.length);
+
+    //Consiguo notación científica
+    nCientifica = nCientifica.substring(0, 1) + "." + nCientifica.substring(2, nCientifica.length);
+    let signo = sig;
+
+    //saco exponente
+    let exponente = decimalBinarioParser((potencia2 + 127));
+
+    //Añado 0 a mantisa
+    while (mantisa.length < 23) {
+
+        mantisa += "0";
+    }
+
+    //muestro por pantalla resultados.
+    alert("Signo = " + signo);
+    alert("Exponente = " + exponente);
+    alert("Mantisa = " + mantisa);
+
+    ieee75432bits = signo.toString() + " " + exponente.toString() + " " + mantisa;
+
+    return ieee75432bits;
+
+}
